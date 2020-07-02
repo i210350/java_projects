038193059b86c40ad1effb18f89e2428718381ef
i210350 <i210350@yandex.ru>
2020-07-02 22:50:46 +0300

    21-33

--- spring-mvc-master/2.2.2. spring_mvc/src/main/java/web/controller/CarController.java
+++ spring-mvc-master/2.2.2. spring_mvc/src/main/java/web/controller/CarController.java
@@ -15,7 +15,7 @@ public class CarController {
 
     @GetMapping(value = "/cars")
     public String printWelcome(ModelMap model) {
-//        List<String> messages = new ArrayList<>();
+        List<String> messages = new ArrayList<>();
 //        messages.add("Hello Car!");
 //        messages.add("I'm Spring MVC application");
 //        messages.add("5.2.0 version by sep'19 ");
