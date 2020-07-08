81b37beeab25635bffac47332e7faa57e8090c9e
i210350 <i210350@yandex.ru>
2020-07-08 20:50:47 +0300

    20-50

---
+++ 2_3_1_crud/src/main/java/web/model/User.java
@@ -0,0 +1,49 @@
+package testgroup.filmography.model;
+
+public class User {
+    private int id;
+    private String name;
+    private String lastname;
+    private int old;
+    private String mail;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public int getOld() {
+        return old;
+    }
+
+    public void setOld(int old) {
+        this.old = old;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+}
