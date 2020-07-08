8d2b940ef0ae02247863bdf307f1ce8fb9ae6e9a
i210350 <i210350@yandex.ru>
2020-07-09 00:12:39 +0300

    20-50

--- 2_3_1_crud/src/main/java/web/controller/UserController.java
+++ 2_3_1_crud/src/main/java/web/controller/UserController.java
@@ -41,10 +41,10 @@ public class UserController {
     }
 
     @RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
-    public ModelAndView editPage(@PathVariable("id") int id) {
+    public ModelAndView editUser(@PathVariable("id") int id) {
         User user = userService.getById(id);
         ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("editPage");
+        modelAndView.setViewName("editUser");
         modelAndView.addObject("user", user);
         return modelAndView;
     }
@@ -52,18 +52,25 @@ public class UserController {
     @RequestMapping(value = "/add", method = RequestMethod.GET)
     public ModelAndView addPage() {
         ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("editPage");
+        modelAndView.setViewName("editUser");
         return modelAndView;
     }
 
     @RequestMapping(value = "/add", method = RequestMethod.POST)
-    public ModelAndView addUser(@ModelAttribute("film") User user) {
+    public ModelAndView addUser(@ModelAttribute("user") User user) {
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("redirect:/");
         userService.add(user);
         return modelAndView;
     }
 
-
+    @RequestMapping(value="/delete/{id}", method = RequestMethod.GET)
+    public ModelAndView deleteUser(@PathVariable("id") int id) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("redirect:/");
+        User user = userService.getById(id);
+        userService.delete(user);
+        return modelAndView;
+    }
 
 }
