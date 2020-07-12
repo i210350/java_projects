2b11c28254cf516757689f3e530dd0268f79a652
i210350 <i210350@yandex.ru>
2020-07-12 11:28:37 +0300

    2

---
+++ 2_4_2_security_1/src/main/java/web/model/User.java
@@ -0,0 +1,77 @@
+package web.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "lastname")
+    private String lastname;
+
+    @Column(name = "old")
+    private int old;
+
+    @Column(name = "mail")
+    private String mail;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public int getOld() {
+        return old;
+    }
+
+    public void setOld(int old) {
+        this.old = old;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", old=" + old +
+                ", mail='" + mail + '\'' +
+                '}';
+    }
+}
---
+++ 2_4_2_security_1/src/main/java/web/service/UserServiceImpl.java
@@ -0,0 +1,49 @@
+package web.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import web.dao.UserDAO;
+import web.model.User;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+    private UserDAO userDAO;
+
+    @Autowired
+    public void setUserDAO(UserDAO userDAO) {
+        this.userDAO = userDAO;
+    }
+
+    @Override
+    @Transactional
+    public List<User> allUsers() {
+        return userDAO.allUsers();
+    }
+
+    @Override
+    @Transactional
+    public void add(User user) {
+        userDAO.add(user);
+    }
+
+    @Override
+    @Transactional
+    public void delete(User user) {
+        userDAO.delete(user);
+    }
+
+    @Override
+    @Transactional
+    public void edit(User user) {
+        userDAO.edit(user);
+    }
+
+    @Override
+    @Transactional
+    public User getById(int id) {
+        return userDAO.getById(id);
+    }
+}
