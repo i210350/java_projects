ac6ce827f2b517c670781ca812219e1b7ec41407
i210350 <i210350@yandex.ru>
2020-07-15 21:38:10 +0300

    2

--- 2_4_2_security_1/src/main/java/web/model/Role.java
+++ 2_4_2_security_1/src/main/java/web/model/Role.java
@@ -22,4 +22,36 @@ public class Role {
     private User user;
 
     public Role() {}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
--- 2_4_2_security_1/src/main/java/web/model/User.java
+++ 2_4_2_security_1/src/main/java/web/model/User.java
@@ -72,6 +72,22 @@ public class User {
         this.mail = mail;
     }
 
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+
     @Override
     public String toString() {
         return "User{" +
