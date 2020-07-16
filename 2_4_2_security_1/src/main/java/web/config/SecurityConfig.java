5d9864e0c48ee550e591aa4b8eb6629cd4b0c197
i210350 <i210350@yandex.ru>
2020-07-16 20:16:42 +0300

    2

--- 2_4_2_security_1/src/main/java/web/config/SecurityConfig.java
+++ 2_4_2_security_1/src/main/java/web/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package web.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -13,6 +14,7 @@ import web.config.handler.LoginSuccessHandler;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
