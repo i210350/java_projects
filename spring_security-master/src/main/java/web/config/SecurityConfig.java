e75620034a36a3e371bf4ee858ef83095c9e9e78
i210350 <i210350@yandex.ru>
2020-07-20 20:39:06 +0300

    20-38

--- spring_security-master/src/main/java/web/config/SecurityConfig.java
+++ spring_security-master/src/main/java/web/config/SecurityConfig.java
@@ -17,7 +17,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("ADMIN").password("111").roles("ADMIN");
+        auth.inMemoryAuthentication().withUser("ADMIN").password("ADMIN").roles("ADMIN");
     }
 
     @Override
