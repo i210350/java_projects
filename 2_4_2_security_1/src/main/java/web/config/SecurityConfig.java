effc96fdf21e207ef67d7d0b726aa55d6ab46231
i210350 <i210350@yandex.ru>
2020-07-20 16:14:09 +0300

    2

--- 2_4_2_security_1/src/main/java/web/config/SecurityConfig.java
+++ 2_4_2_security_1/src/main/java/web/config/SecurityConfig.java
@@ -66,7 +66,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 //страницы аутентификаци доступна всем
                 .antMatchers("/login").anonymous()
                 // защищенные URL
-                .antMatchers("/hello").access("hasAnyRole('ADMIN')").anyRequest().authenticated();
+                .antMatchers("/hello").access("hasAnyRole('manager')").anyRequest().authenticated();
     }
 
 }
---
+++ 2_4_2_security_1/target/2_3_1_crud-1.0-SNAPSHOT/WEB-INF/pages/hello.jsp
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:th="http://www.thymeleaf.org">
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+<body>
+<div>
+<%--    <th:block th:each="msg : ${messages}">--%>
+<%--        <h1 th:text="${msg}"></h1>--%>
+<%--    </th:block>--%>
+    <h4>heLLOO</h4>
+</div>
+</body>
+</html>
