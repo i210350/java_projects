dd7e07101dc9d144c33ce0b7c2651f5db40529ac
i210350 <i210350@yandex.ru>
2020-08-03 23:23:37 +0300

    33

--- user_crud_springboot/src/main/java/com/example/demo/dao/UserDAOImpl.java
+++ user_crud_springboot/src/main/java/com/example/demo/dao/UserDAOImpl.java
@@ -9,29 +9,60 @@ import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
 import javax.persistence.Query;
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Repository
 @Transactional
-public class UserAppDAOImpl {
+public class UserDAOImpl implements UserDAO{
 
     @Autowired
     private EntityManager entityManager;
 
-    public UserApp findById(Long id) {
+
+
+    @Override
+    public List<UserApp> allUsers() {
+        return (List<UserApp>) entityManager.createQuery("FROM UserApp ").getResultList();
+    }
+
+    @Override
+    public void add(UserApp userApp) {
+        entityManager.persist(userApp);
+    }
+
+    @Override
+    public void delete(UserApp userApp) {
+        entityManager.remove(getById(userApp.getId()));
+    }
+
+    @Override
+    public void edit(UserApp userApp) {
+        UserApp user = getById(userApp.getId());
+        user.setName(userApp.getName());
+        user.setLastname(userApp.getLastname());
+        user.setPassword(userApp.getPassword());
+        user.setOld(userApp.getOld());
+        user.setMail(userApp.getMail());
+        entityManager.flush();
+    }
+
+    @Override
+    public UserApp getById(int id) {
         return this.entityManager.find(UserApp.class, id);
     }
 
-    public UserApp findUserAccount(String userName) {
+    @Override
+    public UserApp getByName(String username) {
         try {
-            String sql = "Select e from " + UserApp.class.getName() + " e " //
-                    + " Where e.userName = :userName ";
+            String sql = "select e from UserApp where name = :userName ";
 
             Query query = entityManager.createQuery(sql, UserApp.class);
-            query.setParameter("userName", userName);
+            query.setParameter("userName", username);
 
             return (UserApp) query.getSingleResult();
         } catch (NoResultException e) {
             return null;
         }
     }
+}
 
