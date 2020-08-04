76c6e7cf2a913fd637fbb9d7f9e9df8454f478e6
i210350 <i210350@yandex.ru>
2020-08-04 19:20:59 +0300

    19

---
+++ 311_springboot_22/src/main/java/com/springboot/apllication22/Apllication22Application.java
@@ -0,0 +1,13 @@
+package com.springboot.apllication22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Apllication22Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Apllication22Application.class, args);
+    }
+
+}
---
+++ 311_springboot_22/src/test/java/com/springboot/apllication22/Apllication22ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.apllication22;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Apllication22ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
