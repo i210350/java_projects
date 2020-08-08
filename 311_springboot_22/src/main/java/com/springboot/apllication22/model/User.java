52ab33b45b503c51a298a00c3ae547d08d8496cc
i210350 <i210350@yandex.ru>
2020-08-08 21:41:58 +0300

    19

--- 311_springboot_22/src/main/java/com/springboot/apllication22/model/User.java
+++ 311_springboot_22/src/main/java/com/springboot/apllication22/model/User.java
@@ -22,7 +22,7 @@ import javax.persistence.Table;
 public class User  {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     @Column(name = "firstname")
