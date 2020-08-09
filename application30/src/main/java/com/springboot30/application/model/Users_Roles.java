0bfa8e14c8c14ae932da8be27228ac60dd56ef79
i210350 <i210350@yandex.ru>
2020-08-09 09:33:27 +0300

    22

---
+++ application30/src/main/java/com/springboot30/application/model/Users_Roles.java
@@ -0,0 +1,47 @@
+package com.springboot30.application.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "users_roles")
+public class Users_Roles implements Serializable {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "users_id")
+    private int users_id;
+
+    @Column(name = "roles_id")
+    private int roles_id;
+
+    public Users_Roles() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUsers_id() {
+        return users_id;
+    }
+
+    public void setUsers_id(int users_id) {
+        this.users_id = users_id;
+    }
+
+    public int getRoles_id() {
+        return roles_id;
+    }
+
+    public void setRoles_id(int roles_id) {
+        this.roles_id = roles_id;
+    }
+}
