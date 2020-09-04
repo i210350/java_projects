eb83df702c29a9f7289b73a7557a56901f1be4ee
i210350 <i210350@yandex.ru>
2020-09-04 09:17:36 +0300

    22

--- application313/src/main/java/com/springboot30/application/controller/UserController.java
+++ application313/src/main/java/com/springboot30/application/controller/UserController.java
@@ -60,10 +60,10 @@ public class UserController {
         UserApp user = userServiceImpl.findUserByEmail(auth.getName());
         UserApp deleteUser = userServiceImpl.getUserById(id);
         List<Role> rolesAll = roleServiceImpl.findAllRoles();
-        model.addAttribute("usersList", usersList);
-        model.addAttribute("userAdd", userAdd);
+//        model.addAttribute("usersList", usersList);
+//        model.addAttribute("userAdd", userAdd);
         model.addAttribute("rolesAll", rolesAll);
-        model.addAttribute("userCurrent",user);
+//        model.addAttribute("userCurrent",user);
         model.addAttribute("deleteUser", deleteUser);
         return "/fragments/deleteModal :: deleteModal";
     }
