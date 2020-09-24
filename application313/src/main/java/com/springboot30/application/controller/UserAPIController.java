d587ae6ff1ffbf03d0cfcaf1f28c39c514a6725e
i210350 <i210350@yandex.ru>
2020-09-24 19:23:13 +0300

    22

--- application313/src/main/java/com/springboot30/application/controller/UserAPIController.java
+++ application313/src/main/java/com/springboot30/application/controller/UserAPIController.java
@@ -3,6 +3,7 @@ package com.springboot30.application.controller;
 import com.springboot30.application.model.UserApp;
 import com.springboot30.application.repository.RoleRepository;
 import com.springboot30.application.repository.UserRepository;
+import com.springboot30.application.repository.UserService;
 import com.springboot30.application.service.RoleServiceImpl;
 import com.springboot30.application.service.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,41 +23,42 @@ import java.util.List;
 @RequestMapping("/api")
 public class UserAPIController {
     @Autowired
-    UserServiceImpl userServiceImpl;
+    UserService userService;
 
     @GetMapping("/{id}")
     public UserApp getUserById(@PathVariable("id") Long id) {
 //            throws RecordNotFoundException {
-        UserApp userApp = userServiceImpl.getUserById(id);
+        UserApp userApp = userService.getUserById(id);
 
         return userApp;
     }
 
     @PostMapping("/add")
     void addUser(@RequestBody UserApp newUser) {
-        userServiceImpl.saveUser(newUser);
+        userService.saveUser(newUser);
     }
 
     @PutMapping("/edit")
     void editUser(@RequestBody UserApp editUser) {
-        userServiceImpl.saveUser(editUser);
+        userService.saveUser(editUser);
     }
 
     @DeleteMapping("/delete")
     void deleteUser(@RequestBody UserApp deleteUser) {
-        userServiceImpl.deleteUser(deleteUser);
+        userService.deleteUser(deleteUser);
     }
 
     @GetMapping("/users")
     public List<UserApp> getAllUsers() {
-        List<UserApp> usersApp = userServiceImpl.getAllByActive(1);
+        List<UserApp> usersApp = userService.getAllByActive(1);
         return usersApp;
     }
 
     @GetMapping("/currentUser")
     public List<UserApp> getCurrentUser() {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        UserApp user = userServiceImpl.findUserByEmail(auth.getName());
+//        UserApp user = userService.findUserByEmail(auth.getName());
+        UserApp user = userService.findUserByEmail(auth.getName());
         List<UserApp> usersList = new ArrayList<>();
         usersList.add(user);
         return usersList;
